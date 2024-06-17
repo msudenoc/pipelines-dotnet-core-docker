d5faa4e531d50f649fd1e61cc4ca485acc068c11
Mihail Sudenoc <msudenoc@MD-IT50006>
2024-06-17 08:42:58 +0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace pipelines_dotnet_core_docker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
